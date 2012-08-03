c37a751064b596714be13f8cf9f3b82d31c308ee
cas <cas@ubuntu>
2012-08-03 23:01:01 +0300

    Commit skipped files

--- src/main/java/ss/hotel/android/HelloAndroidActivity.java
+++ src/main/java/ss/hotel/android/HelloAndroidActivity.java
@@ -1,25 +1,35 @@
 package ss.hotel.android;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
+
 
 public class HelloAndroidActivity extends Activity {
-
-    private static String TAG = "android";
-
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
         setContentView(R.layout.main);
-    }
+        TextView springmessage = (TextView) findViewById(R.id.springmessage);
 
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(new MediaType("application","json"));
+        HttpEntity<String> requestEntity = new HttpEntity<String>(requestHeaders);
+        String url = "http://85.198.176.13:8080/hotel/api";
+        RestTemplate restTemplate = new RestTemplate(true);
+    	ResponseEntity<String> responseEntity =
+    			restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+        String result = responseEntity.getBody();
+        springmessage.setText(result);
+
+    }
 }
 
